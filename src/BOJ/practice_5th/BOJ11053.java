10cee40442dec287965a8953dcb250f928113239
parkdoyoung <ehdud5578@gmail.com>
2021-10-27 22:59:10 +0900

    add : lis dp

--- src/BOJ/practice_5th/BOJ11053.java
+++ src/BOJ/practice_5th/BOJ11053.java
@@ -7,26 +7,36 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ11053 {
+    static int[] array,dp;
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] array = new int[n];
+        array = new int[n];
+        dp = new int[n];
         int[] cnt = new int[n];
         Arrays.fill(cnt,0);
         StringTokenizer stz = new StringTokenizer(br.readLine());
         for(int i = 0;i<n;i++){
             array[i] = Integer.parseInt(stz.nextToken());
         }
-        for(int i = n-1;i>0;i--){
-            for(int j = i-1;j>=0;j--){
-                if(array[j]<array[i])
-                    cnt[j]++;
+        for(int i = 0;i<n;i++){
+            lis(i);
+        }
+        int result = 0;
+        for(int i = 0;i<n;i++){
+            result = Math.max(result,dp[i]);
+        }
+        System.out.println(result);
+    }
+    public static int lis(int n){
+        if(dp[n]==0){
+            dp[n] = 1;
+            for(int i = n-1;i>=0;i--){
+                if(array[i]<array[n]){
+                    dp[n] = Math.max(dp[n] ,lis(i) + 1);
+                }
             }
         }
-        int maxvalue = 0;
-        for(int i = 0;i<n;i++){
-            if(cnt[i]>maxvalue)maxvalue = cnt[i];
-        }
-        System.out.println(maxvalue);
+        return dp[n];
     }
 }
