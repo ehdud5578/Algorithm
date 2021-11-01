b1a2172e219580e6acb0f9d086ba16b972a1d73c
parkdoyoung <ehdud5578@gmail.com>
2021-11-01 19:57:59 +0900

    add : dp

---
+++ src/BOJ/BOJ12904.java
@@ -0,0 +1,26 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ12904 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer s = new StringBuffer(br.readLine());
+        StringBuffer t = new StringBuffer(br.readLine());
+        while (s.length() < t.length()){
+            if(t.charAt(t.length()-1) == 'A'){
+                t.deleteCharAt(t.length()-1);
+            }else if(t.charAt(t.length() - 1) == 'B'){
+                t.deleteCharAt(t.length() - 1);
+                t.reverse();
+            }
+        }
+
+        if(s.toString().equals(t.toString())){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
