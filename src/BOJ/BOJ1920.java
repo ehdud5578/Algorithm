9db2518541d21cc32bd3bcc703db818b46bb7d4b
parkdoyoung <ehdud5578@gmail.com>
2021-11-03 10:21:13 +0900

    add : BOJ1920

--- src/BOJ/BOJ1920.java
+++ src/BOJ/BOJ1920.java
@@ -1,7 +1,29 @@
 package BOJ;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
 public class BOJ1920 {
     public static void main(String[] args) throws Exception{
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<Integer> array = new HashSet<>();
+        StringTokenizer stz = new StringTokenizer(br.readLine());
+        for(int i = 0;i<n;i++){
+            int a = Integer.parseInt(stz.nextToken());
+            array.add(a);
+        }
+        int m = Integer.parseInt(br.readLine());
+        stz = new StringTokenizer(br.readLine());
+        for (int i = 0;i<m;i++){
+            int temp = Integer.parseInt(stz.nextToken());
+            if(array.contains(temp))
+                System.out.println(1);
+            else
+                System.out.println(0);
+        }
     }
 }
