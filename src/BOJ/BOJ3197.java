fed31c10c3c65a83adba95e0cae41116663dfa59
parkdoyoung <ehdud5578@gmail.com>
2021-11-04 10:13:43 +0900

    add : BOJ1920 드디어 성공

--- src/BOJ/BOJ3197.java
+++ src/BOJ/BOJ3197.java
@@ -8,83 +8,84 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ3197 {
-    static int r,c;
+    static int r, c;
     static char[][] lake;
-    static Queue<int[]> water,queue;
-    static int[] dx,dy;
-    static Point start,end;
+    static Queue<int[]> water, queue;
+    static int[] dx, dy;
+    static Point start, end;
     static boolean[][] visited;
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stz = new StringTokenizer(br.readLine());
         r = Integer.parseInt(stz.nextToken());
         c = Integer.parseInt(stz.nextToken());
-        dx = new int[]{0,1,0,-1};
-        dy = new int[]{1,0,-1,0};
+        dx = new int[]{0, 1, 0, -1};
+        dy = new int[]{1, 0, -1, 0};
         lake = new char[r][c];
         water = new LinkedList<>();
         visited = new boolean[r][c];
         queue = new LinkedList<>();
         boolean check = false;
-        for(int i = 0;i<r;i++){
+        for (int i = 0; i < r; i++) {
             char[] temp = br.readLine().toCharArray();
-            for(int j = 0;j<c;j++){
+            for (int j = 0; j < c; j++) {
                 lake[i][j] = temp[j];
-                if(temp[j] == 'L'){
-                    if(!check){
-                        start = new Point(j,i);
+                if (temp[j] == 'L') {
+                    if (!check) {
+                        start = new Point(j, i);
                         check = true;
-                    }
-                    else
-                        end = new Point(j,i);
+                    } else
+                        end = new Point(j, i);
                     lake[i][j] = '.';
-                }else if(temp[j] == '.'){
-                    water.add(new int[]{j,i});
+                }
+                if (lake[i][j] == '.') {
+                    water.add(new int[]{j, i});
                 }
             }
         }
-        queue.add(new int[]{start.x,start.y});
+        queue.add(new int[]{start.x, start.y});
         visited[start.y][start.x] = true;
         int result = 0;
-        while (!isConnected()){
+        while (!isConnected()) {
             result++;
             meltice();
         }
         System.out.println(result);
     }
 
-    public static void meltice(){
+    public static void meltice() {
         int size = water.size();
-        for(int i = 0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             int[] now = water.poll();
-            for(int j = 0;j<4;j++){
+            for (int j = 0; j < 4; j++) {
                 int ny = now[1] + dy[j]; // NullPointerException 경고.
                 int nx = now[0] + dx[j];
-                if(nx>=0&&nx<c&&ny>=0&&ny<r && lake[ny][nx] == 'X'){
-                    water.add(new int[]{nx,ny});
+                if (nx >= 0 && nx < c && ny >= 0 && ny < r && lake[ny][nx] == 'X') {
+                    water.add(new int[]{nx, ny});
                     lake[ny][nx] = '.';
                 }
             }
         }
     }
 
-    public static boolean isConnected(){
+    public static boolean isConnected() {
         Queue<int[]> nextQueue = new LinkedList<>();
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int[] now = queue.poll();
-            if(now[0]==end.x && now[1] ==end.y)
+            if (now[0] == end.x && now[1] == end.y)
                 return true;
-            for(int i = 0;i<4;i++){
+            for (int i = 0; i < 4; i++) {
                 int nx = now[0] + dx[i];
                 int ny = now[1] + dy[i];
-                if(nx>=0&&nx<c&&ny>=0&&ny<r){
-                    if(!visited[ny][nx]&& lake[ny][nx] == '.') { // 방문하지 않았고 물일때 계속 탐색
-                        queue.add(new int[]{nx,ny});
-                        visited[ny][nx] = true;
-                    } else{
-                        nextQueue.add(new int[]{nx,ny}); // 얼음일 경우 다음턴부터 탐색 함.
-                        visited[ny][nx] = true;
+                if (nx >= 0 && nx < c && ny >= 0 && ny < r && !visited[ny][nx]) {
+                    if (lake[ny][nx] == '.') { // 방문하지 않았고 물일때 계속 탐색
+                        queue.add(new int[]{nx, ny});
                     }
+                    if (lake[ny][nx] == 'X') {
+                        nextQueue.add(new int[]{nx, ny}); // 얼음일 경우 다음턴부터 탐색 함.
+                    }
+                    visited[ny][nx] = true;
                 }
             }
         }
@@ -99,4 +100,21 @@ public class BOJ3197 {
 *
 * 메모리 초과...
 * 다른사람들 블로그와 비교해 봐도 왜 틀렸는지 모르겠다
+* 핵심로직이나 매소드 등이 거의 비슷하지만 난 메모리 초과이다.
+*
+* https://herong.tistory.com/entry/BOJ-3197-%EB%B0%B1%EC%A1%B0%EC%9D%98-%ED%98%B8%EC%88%98-Java
+* if(nx>=0&&nx<c&&ny>=0&&ny<r ){
+                    if(!visited[ny][nx]&&lake[ny][nx] == '.') { // 방문하지 않았고 물일때 계속 탐색
+                        queue.add(new int[]{nx,ny});
+                    } else if(lake[ny][nx] == 'X'){
+                        nextQueue.add(new int[]{nx,ny}); // 얼음일 경우 다음턴부터 탐색 함.
+                    }
+                    visited[ny][nx] = true;
+                }
+               * 이렇게 될 경우 방문했던 곳을 계속해서 체크하기 때문에 잘못된 방법이었다.
+* * 55% 실패
+* 처음에 입력을 받는 과정에서 백조가 있는 자리를 start , end 에 저장을 한뒤
+* 물이 있는곳으로 넣어줘야하는데 그 과정을 빠뜨려서 오래걸림...
+* 오늘또한 멍청한 실수를 했기 때문에 오래걸렸다...
+*
 * */
